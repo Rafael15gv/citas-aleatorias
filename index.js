2c2b0e389d03f597d8345349f104dfb0cdc9f5d9
Rafael15gv <143367099+Rafael15gv@users.noreply.github.com.>
2024-10-21 16:44:28 -0500

    se modifica index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const titulo = document.getElementsByTagName("h2")[0];
 
 boton.addEventListener("click", () => {
   let indice = Math.floor(Math.random() * 10);
+  
 
   console.log(citas[indice].autor);
   console.log(citas[indice].texto);
